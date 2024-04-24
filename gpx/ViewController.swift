036af2be683825ab03fbd65783bef5c45e444c52
gaoguangxiao <gaoguangxiao125@sina.com>
2024-04-24 17:59:12 +0800

    虚拟定位

---
+++ gpx/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  gpx
+//
+//  Created by 高广校 on 2024/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+//        WGS84 坐标系统
+        
+//        地图转化坐标
+//    https://tool.lu/coordinate/
+    }
+
+
+}
+
